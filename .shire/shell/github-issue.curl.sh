5bb6e7e29d22b1c9e05c44f951b0b3337c199592
Phodal Huang <h@phodal.com>
2024-09-02 15:42:33 +0800

    fix(github-api): 优化GitHub issue获取和controller提示

--- .shire/shell/github-issue.curl.sh
+++ .shire/shell/github-issue.curl.sh
@@ -1,5 +1,3 @@
-# 使用curl获取GitHub issue
-curl -H "Authorization: token ${TOKEN}" \
-     -H "Accept: application/vnd.github.v3+json" \
-     https://api.github.com/repos/unit-mesh/untitled/issues/10
-
+curl --location 'https://api.github.com/repos/unit-mesh/untitled/issues/10' \
+  -H "Authorization: token ${TOKEN}" \
+  -H "Accept: application/vnd.github.v3+json" \
