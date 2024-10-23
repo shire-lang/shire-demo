5834884661b8801af7e2f849970396b8b69ea619
Phodal Huang <h@phodal.com>
2024-10-23 22:39:03 +0800

    chore(dify-epic-story): swap 'feature' and 'story_list' inputs order in curl request

--- .shire/shell/dify-epic-story.curl.sh
+++ .shire/shell/dify-epic-story.curl.sh
@@ -2,7 +2,7 @@ curl -X POST 'https://api.dify.ai/v1/completion-messages' \
   --header "Authorization: Bearer ${epicStory}" \
   --header 'Content-Type: application/json' \
   --data-raw '{
-      "inputs": {"feature": "Hello, world!", "story_list": ${storyList}},
+      "inputs": {"story_list": ${story}, "feature": "Hello, world!"},
       "response_mode": "streaming",
       "user": "phodal"
   }'
